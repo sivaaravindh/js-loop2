2549e5934f3aa57a00005882bcbd60840e0f795a
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-14 08:17:26 -0800

    index

---
+++ main.js
@@ -0,0 +1,10 @@
+let num=parseInt (prompt("Enter the number"));
+
+for (let i=2; i<=num; i++){
+        
+    for(let j=2; j<=i; j++){
+    
+        document.write((i-1))
+    }
+    document.write("<br>")
+}
